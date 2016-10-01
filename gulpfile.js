6abf8b737148fcdce99e7494bb3b05d2e3bad0dd
WRQQQ <wrqqqrrr@rambler.ru>
2016-10-01 19:10:48 +0300

    Compile and reloading sass on save, site header

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var less = require('gulp-less');
+var watch = require('gulp-watch');
 
 gulp.task('less', function() {
   return gulp.src('./css/*.less')
@@ -9,4 +10,8 @@ gulp.task('less', function() {
       .pipe(gulp.dest('./output'));
 });
 
-gulp.task('default', ['less']);
+gulp.task('watch-less', function() {  
+  gulp.watch('css/*.less' , ['less']);
+});
+
+gulp.task('default', ['less' , 'watch-less']);
